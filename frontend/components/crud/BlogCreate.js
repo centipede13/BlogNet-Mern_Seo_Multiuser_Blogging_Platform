c8fb7b60824e4e432f6a0116bbb45851c1041067
centipede13 <aryanpandey748@gmail.com>
2020-12-21 20:22:40 +0530

    Added Categories & Tags to BlogCreate Form

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -24,6 +24,13 @@ const CreateBlog = ({ router }) => {
     }
   };
 
+  // state functions
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  const [checked, setChecked] = useState([]);
+  const [checkedTag, setCheckedTag] = useState([]);
+
   const [body, setBody] = useState(blogFromLS);
   const [values, setValues] = useState({
     error: "",
@@ -45,7 +52,28 @@ const CreateBlog = ({ router }) => {
 
   useEffect(() => {
     setValues({ ...values, formData: new FormData() });
+    initCategories();
+    initTags();
   }, [router]);
+
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+  const initTags = () => {
+    getTags().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setTags(data);
+      }
+    });
+  };
 
   const publishBlog = (e) => {
     e.preventDefault();
@@ -68,6 +96,73 @@ const CreateBlog = ({ router }) => {
     }
   };
 
+  // Handle Categories Checkbox Toggle
+  const handleToggle = (c) => () => {
+    setValues({ ...values, error: "" });
+    // will return first  index or -1
+    const clickedCategory = checked.indexOf(c);
+    const all = [...checked];
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    console.log(all);
+    setChecked(all);
+    formData.set("categories", all);
+  };
+
+  // Categories CheckBox
+  const showCategories = () => {
+    return (
+      categories &&
+      categories.map((c, i) => (
+        <li key={i} className="list-unstyled">
+          <input
+            onChange={handleToggle(c._id)}
+            type="checkbox"
+            className="mr-2"
+          />
+          <label className="form-check-label">{c.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  // Handle Tags Checkbox Toggle
+  const handleTagsToggle = (t) => () => {
+    setValues({ ...values, error: "" });
+    // will return first  index or -1
+    const clickedTag = checkedTag.indexOf(t);
+    const all = [...checkedTag];
+    if (clickedTag === -1) {
+      all.push(t);
+    } else {
+      all.splice(clickedTag, 1);
+    }
+    console.log(all);
+    setCheckedTag(all);
+    formData.set("tags", all);
+  };
+
+  // Tags CheckBox
+  const showTags = () => {
+    return (
+      tags &&
+      tags.map((t, i) => (
+        <li key={i} className="list-unstyled">
+          <input
+            onChange={handleTagsToggle(t._id)}
+            type="checkbox"
+            className="mr-2"
+          />
+          <label className="form-check-label">{t.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  // Blog Creation Form
   const createBlogForm = () => {
     return (
       <form onSubmit={publishBlog}>
@@ -101,12 +196,36 @@ const CreateBlog = ({ router }) => {
   };
 
   return (
-    <div>
-      {createBlogForm()}
-      <hr />
-      {JSON.stringify(title)}
-      <hr />
-      {JSON.stringify(body)}
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8">
+          {createBlogForm()}
+          <hr />
+          {JSON.stringify(title)}
+          <hr />
+          {JSON.stringify(body)}
+          <hr />
+          {JSON.stringify(categories)}
+          <hr />
+          {JSON.stringify(tags)}
+        </div>
+        <div className="col-md-4">
+          <div>
+            <h5>Categories</h5>
+            <hr />
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showCategories()}
+            </ul>
+          </div>
+          <div>
+            <h5>Tags</h5>
+            <hr />
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showTags()}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
