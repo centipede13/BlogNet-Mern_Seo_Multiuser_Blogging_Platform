349721d7380f6c552e4719fee3ae522ff1ab1bd9
centipede13 <aryanpandey748@gmail.com>
2020-12-22 17:08:37 +0530

    implemented list all blogs api

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const { create } = require("../controllers/blog");
+const {
+  create,
+  list,
+  listAllBlogsCategoriesTags,
+  read,
+  remove,
+  update,
+} = require("../controllers/blog");
 const { requireSignin, adminMiddleWare } = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/blog", requireSignin, adminMiddleWare, create);
+router.get("/blogs", list);
+router.post("/blogs-categories-tags", listAllBlogsCategoriesTags);
+router.get("/blog/:slug", read);
+router.delete("/blog/:slug", requireSignin, adminMiddleWare, remove);
+router.put("/blog/:slug", requireSignin, adminMiddleWare, update);
 
 module.exports = router;
