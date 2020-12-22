7380404547e58644eaa65a1eab6423827f7c12c8
centipede13 <aryanpandey748@gmail.com>
2020-12-22 17:38:44 +0530

    implimented listAllBlogsCategoriesTags

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -124,7 +124,58 @@ exports.list = (req, res) => {
     });
 };
 
-exports.listAllBlogsCategoriesTags = (req, res) => {};
+exports.listAllBlogsCategoriesTags = (req, res) => {
+  let limit = req.body.limit ? parseInt(req.body.limit) : 10;
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+  let blogs;
+  let categories;
+  let tags;
+
+  Blog.find({})
+    .populate("categories", "_id name slug")
+    .populate("tags", "_id name slug")
+    .populate("postedBy", "_id name username profile")
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit)
+    .select(
+      "_id title slug excerpt categories tags postedBy createdAt updatedAt"
+    )
+    .exec((err, data) => {
+      if (err) {
+        return res.json({
+          error: errorHandler(err),
+        });
+      }
+
+      // Getting all blogs
+      blogs = data;
+
+      // Getting all categories
+      Category.find({}).exec((err, c) => {
+        if (err) {
+          return res.json({
+            error: errorHandler(err),
+          });
+        }
+        categories = c;
+
+        // Getting all tags
+        Tag.find({}).exec((err, t) => {
+          if (err) {
+            return res.json({
+              error: errorHandler(err),
+            });
+          }
+          tags = t;
+
+          // Return all blogs, categories and tags
+          res.json({ blogs, categories, tags, size: blogs.length });
+        });
+      });
+    });
+};
 
 exports.read = (req, res) => {};
 
