537ab8c45487dfc1f908cb2d551479a7c32a287e
centipede13 <aryanpandey748@gmail.com>
2020-12-24 13:12:20 +0530

    moved blogs  to Card component

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,46 +1,16 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "../../components/Layout";
 import { useState } from "react";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
-import { API } from "../../config";
-import renderHTML from "react-render-html";
-import moment from "moment";
+
+import Card from "../../components/blog/Card";
 
 const Blogs = ({ blogs, categories, tags, size }) => {
   const showAllBlogs = () => {
     return blogs.map((blog, i) => {
       return (
         <article key={i}>
-          <div className="lead pb-4">
-            <header>
-              <Link href={`/blogs/${blog.slug}`}>
-                <a>
-                  <h2 className="pt-3 pb-3 font-weight-bold">{blog.title}</h2>
-                </a>
-              </Link>
-            </header>
-            <section>
-              <p className="mark ml-1 pt-2 pb-2">
-                Written By: {blog.postedBy.name} | Published On:{" "}
-                {moment(blog.updatedAt).fromNow()}
-              </p>
-            </section>
-            <section>
-              <p>Blog categories & tags</p>
-            </section>
-            <div className="row">
-              <div className="col-md-4">Image</div>
-              <div className="col-md-8">
-                <section>
-                  <div className="pb-3">{renderHTML(blog.excerpt)}</div>
-                  <Link href={`/blogs/${blog.slug}`}>
-                    <a className="btn btn-primary pt-2">Read more</a>
-                  </Link>
-                </section>
-              </div>
-            </div>
-          </div>
+          <Card blog={blog} />
           <hr />
         </article>
       );
