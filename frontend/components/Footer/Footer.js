8e1423659141a48cb5af5fae5014d790aefab3c4
centipede13 <aryanpandey748@gmail.com>
2021-01-16 18:35:30 +0530

    Made changes to n progress bar styling

--- frontend/components/Footer/Footer.js
+++ frontend/components/Footer/Footer.js
@@ -1,15 +1,15 @@
 const Footer = () => (
   <footer className="footer">
     <div className="footer__copyright">
-      <p>Copyright © 2020 BloggingCoder.com</p>
+      <p>Copyright © 2020 BlogNet.com</p>
       <p className="footer__text-and-author">
-        All rights reserved. Made with ❤ in London, UK, by{" "}
+        All rights reserved. Made with ❤ in Delhi, India, by{" "}
         <a
           className="footer__author"
           target="_blank"
-          href="https://alexticovschi.netlify.app/"
+          href="http://localhost:3000/"
         >
-          Alex Ticovschi
+          Aryan Pandey
         </a>
       </p>
     </div>
